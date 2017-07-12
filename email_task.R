c6718a5ae87388299a3864493369f7d7e8a2f16f
Yiming-Gao <gaoyiming0418@gmail.com>
2017-07-12 14:18:21 -0500

    Update email_task.R

--- email_task.R
+++ email_task.R
@@ -117,7 +117,7 @@ leaflet(plot_data_0313) %>% addTiles() %>%
     options = layersControlOptions(collapsed = FALSE)) %>% hideGroup("Traffic density = 1")
 
 # 2b. map of average speed
-plot_data_0313 = temp_0313 %>% dplyr::select(longitude, latitude, avg_speed) %>% distinct(longitude, latitude, avg_speed)
+plot_data_0313 = temp_0313 %>% dplyr::select(longitude, latitude, traffic_den_day, avg_speed) %>% distinct(longitude, latitude, traffic_den_day, avg_speed)
 
 pal <- colorNumeric(
   palette = "Reds",
@@ -138,25 +138,33 @@ temp2$Mode = as.factor(ifelse(temp2$lonG > 0, "Acceleration", "Deceleration"))
 
 # 1di
 speed_plot_smooth1 <- function(trip_number) {
-  plot_data = temp2[temp2$trip_number == trip_number, ]
+  plot_data = temp2[temp2$trip_number == "00EB796E3FD14E5FA5C3EC1C3089B2B800", ][1: 764, ]
   plot_data_label_index = ceiling(seq(1, nrow(plot_data), length.out = 20))
   plot_data_breaks = plot_data$Time[plot_data_label_index]
   plot_data_labels = (paste0(plot_data$Hour[plot_data_label_index], ":",
                              plot_data$Minute[plot_data_label_index], ":",
                              round(plot_data$Second[plot_data_label_index], 0), " ",
                              plot_data$label))[1: 20]
+  plot_data$traffic_den_smooth = c(rollapply(plot_data$traffic_den, width = 10, mean, fill = 0))
   
   ggplot(plot_data, aes(Time)) + 
     scale_x_continuous(breaks = plot_data_breaks, labels = plot_data_labels) +
-    scale_y_continuous(name = "Speed", breaks = seq(0, 100, 10), sec.axis = sec_axis(~./10, breaks = seq(0, 10, 1), name = "Traffic Density")) + 
-    geom_smooth(aes(y = speed, colour = "Vehicle speed"), se = FALSE, span = 0.4) + 
-    geom_smooth(aes(y = speed_lim, colour = "Predicted speed limit"), se = FALSE, span = 0.4) +
-    ggtitle(paste("Trip number: ", trip_number)) + ylab("Speed") +
-    labs(colour = "Variable") + 
-    geom_smooth(aes(y = traffic_den * 10, colour = "Traffic density"), se = FALSE, span = 0.4) +
-    theme(panel.background = element_rect(fill = 'gray93'), 
+    scale_y_continuous(name = "Speed", breaks = seq(0, 100, 10), sec.axis = sec_axis(~./(60 / 14), breaks = seq(0, 15, 1), name = "Traffic Density")) + 
+    geom_line(aes(y = speed, colour = "Vehicle speed"), size = 0.8) + 
+    geom_line(aes(y = speed_lim_smooth, colour = "Predicted speed limit"), size = 0.8) +
+    geom_smooth(aes(y = avg_speed, colour = "Average road speed"), se = FALSE, span = 0.2) + 
+    geom_point(aes(y = speed, colour = Mode1)) +
+    ggtitle(paste("Trip number: ", "00EB796E3FD14E5FA5C3EC1C3089B2B800")) + ylab("Speed") +
+    geom_ribbon(aes(ymin = rep(0, 764), ymax = traffic_den_smooth * (60 / 14)), alpha = 0.3, fill = 'gray65') + 
+    scale_color_manual(name = "Variables", 
+                       values = c("Vehicle speed" = "dodgerblue2", 
+                                  "Predicted speed limit" = "indianred1", 
+                                  "Average road speed" = "goldenrod2", 
+                                  "Acceleration" = "green", 
+                                  "Deceleration" = "red1")) + 
+    theme(panel.background = element_rect(fill = 'gray98'), 
           axis.text.x = element_text(angle = 45, hjust = 1),
-          legend.position = c(0.85, 0.8))}
+          legend.position = c(0.9, 0.85))}
 
 # speed_plot_smooth1("17074D96C94C4CF19C82309E39539A1D00")
 
@@ -211,7 +219,7 @@ acceleration_plot1 <- function(trip_number) {
 
 # 2di
 speed_plot_smooth2 <- function(trip_number) {
-  plot_data = temp2[temp2$trip_number == trip_number, ]
+  plot_data = temp2[temp2$trip_number == trip_number, ][1: 764, ]
   plot_data_label_index = ceiling(seq(1, nrow(plot_data), length.out = 20))
   plot_data_breaks = plot_data$Time[plot_data_label_index]
   plot_data_labels = (paste0(plot_data$Hour[plot_data_label_index], ":",
@@ -221,19 +229,21 @@ speed_plot_smooth2 <- function(trip_number) {
   
   ggplot(plot_data, aes(Time)) + 
     scale_x_continuous(breaks = plot_data_breaks, labels = plot_data_labels) +
-    geom_smooth(aes(y = speed, colour = "Vehicle speed"), se = FALSE, span = 0.4) + 
-    geom_smooth(aes(y = speed_lim, colour = "Predicted speed limit"), se = FALSE, span = 0.4) +
+    geom_line(aes(y = speed, colour = "dodgerblue2"), size = 0.8) + 
+    geom_line(aes(y = speed_lim_smooth, colour = "indianred1"), size = 0.8) +
     ggtitle(paste("Trip number: ", trip_number)) + ylab("Speed") +
     labs(colour = "Variable") + 
-    geom_smooth(aes(y = avg_speed, colour = "Average speed"), se = FALSE, span = 0.4) +
+    scale_color_manual(labels = c("Vehicle speed", "Predicted speed limit", "Average road speed"), values = c("dodgerblue2", "indianred1", "seagreen3")) + 
+    geom_smooth(aes(y = avg_speed, colour = "seagreen3"), se = FALSE, span = 0.4) +
     theme(panel.background = element_rect(fill = 'gray93'), 
           axis.text.x = element_text(angle = 45, hjust = 1),
-          legend.position = c(0.15, 0.8))}
+          legend.position = c(0.85, 0.8))}
 
 # speed_plot_smooth2("17074D96C94C4CF19C82309E39539A1D00")
+# speed_plot_smooth2("00EB796E3FD14E5FA5C3EC1C3089B2B800")
 
 speed_plot_line2 <- function(trip_number) {
-  plot_data = temp2[temp2$trip_number == trip_number, ]
+  plot_data = temp2[temp2$trip_number == trip_number, ][1: 764, ]
   plot_data_label_index = ceiling(seq(1, nrow(plot_data), length.out = 20))
   plot_data_breaks = plot_data$Time[plot_data_label_index]
   plot_data_labels = (paste0(plot_data$Hour[plot_data_label_index], ":",
@@ -244,19 +254,19 @@ speed_plot_line2 <- function(trip_number) {
   ggplot(plot_data, aes(Time)) + 
     scale_x_continuous(breaks = plot_data_breaks, labels = plot_data_labels) +
     geom_line(aes(y = speed, colour = "Vehicle speed"), size = 0.8) + 
-    geom_line(aes(y = speed_lim, colour = "Predicted speed limit"), size = 0.8) +
+    geom_line(aes(y = speed_lim_smooth, colour = "Predicted speed limit"), size = 0.8) +
     ggtitle(paste("Trip number: ", trip_number)) + ylab("Speed") +
     labs(colour = "Variable") + 
     geom_line(aes(y = avg_speed, colour = "Average speed"), size = 0.8) +
     theme(panel.background = element_rect(fill = 'gray93'), 
           axis.text.x = element_text(angle = 45, hjust = 1),
-          legend.position = c(0.15, 0.8))}
+          legend.position = c(0.85, 0.8))}
 
-# speed_plot_line2("17074D96C94C4CF19C82309E39539A1D00")
+# speed_plot_line2("00EB796E3FD14E5FA5C3EC1C3089B2B800")
 
 # 2dii
 acceleration_plot2 <- function(trip_number) {
-  plot_data = temp2[temp2$trip_number == trip_number, ]
+  plot_data = temp2[temp2$trip_number == trip_number, ][1: 764, ]
   plot_data_label_index = ceiling(seq(1, nrow(plot_data), length.out = 20))
   plot_data_breaks = plot_data$Time[plot_data_label_index]
   plot_data_labels = (paste0(plot_data$Hour[plot_data_label_index], ":",
@@ -267,17 +277,64 @@ acceleration_plot2 <- function(trip_number) {
   ggplot(plot_data, aes(Time)) + 
     scale_x_continuous(breaks = plot_data_breaks, labels = plot_data_labels) +
     geom_point(aes(y = speed, colour = Mode)) + 
-    geom_smooth(aes(y = speed_lim, colour = "Predicted speed limit"), se = FALSE, span = 0.4) +
+    geom_smooth(aes(y = speed_lim_smooth, colour = "indianred1"), se = FALSE, span = 0.4) +
     ggtitle(paste("Trip number: ", trip_number)) + ylab("Speed") +
     labs(colour = "Variable") + 
-    geom_smooth(aes(y = avg_speed, colour = "Average speed"), se = FALSE, span = 0.4) +
+    scale_color_manual(labels = c("Acceleration", "Deceleration", "Predicted speed limit", "Average road speed"), values = c("green3", "red1", "indianred1", "seagreen3")) + 
+    geom_smooth(aes(y = avg_speed, colour = "seagreen3"), se = FALSE, span = 0.4) +
     theme(panel.background = element_rect(fill = 'gray93'), 
           axis.text.x = element_text(angle = 45, hjust = 1),
-          legend.position = c(0.15, 0.8))}
+          legend.position = c(0.85, 0.8))}
 
 # acceleration_plot2("17074D96C94C4CF19C82309E39539A1D00")
 # acceleration_plot2("02C6D1CFCCF0404B89C0ED4A0EF507DC00")
 # acceleration_plot2("0E3CE647C8714AF7A2243AEEBF03A5C300")
+
+
+
+
+# "vehicle speed": blue line, consider 0.2g as "acceleration"
+temp2$Mode1 = as.factor(ifelse(temp2$lonG >= 0.15, "Acceleration", ifelse(temp2$lonG <= -0.15, "Deceleration", NA)))
+temp2$Mode2 = as.factor(ifelse(temp2$lonG >= 0.2, "Acceleration", ifelse(temp2$lonG <= -0.2, "Deceleration", NA)))
+
+acceleration_plot3 <- function(trip_number) {
+  plot_data = temp2[temp2$trip_number == trip_number, ][1: 764, ]
+  plot_data_label_index = ceiling(seq(1, nrow(plot_data), length.out = 20))
+  plot_data_breaks = plot_data$Time[plot_data_label_index]
+  plot_data_labels = (paste0(plot_data$Hour[plot_data_label_index], ":",
+                             plot_data$Minute[plot_data_label_index], ":",
+                             round(plot_data$Second[plot_data_label_index], 0), " ",
+                             plot_data$label))[1: 20]
+  
+  ggplot(plot_data, aes(Time)) + 
+    scale_x_continuous(breaks = plot_data_breaks, labels = plot_data_labels) +
+    geom_line(aes(y = speed, colour = "dodgerblue"), size = 0.8) + 
+    geom_point(aes(y = speed, colour = Mode2)) + 
+    ggtitle(paste("Trip number: ", trip_number)) + ylab("Speed") +
+    labs(colour = "Variable") + 
+    scale_color_manual(labels = c("Acceleration", "Deceleration", "Constant speed", "Vehicle Speed"), values = c("green1", "firebrick1", "gray50", "dodgerblue")) + 
+    theme(panel.background = element_rect(fill = 'gray93'), 
+          axis.text.x = element_text(angle = 45, hjust = 1),
+          legend.position = c(0.85, 0.8))}
+
+# acceleration_plot3("00EB796E3FD14E5FA5C3EC1C3089B2B800")
+
+
+# most frequent value
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
+temp2$speed_lim_smooth = (temp2 %>% mutate(speed_lim_temp = c(rollapply(speed_lim, width = 15, mean, fill = NA))) %>%
+                            mutate(speed_lim_smooth = ifelse(speed_lim_temp <= (20 + 35) / 2, 20,
+                                                             ifelse(speed_lim_temp <= (35 + 55) / 2, 35, 
+                                                                    ifelse(speed_lim_temp <= (55 + 65) / 2, 55,
+                                                                           ifelse(speed_lim_temp <= (65 + 70) / 2, 65, 70))))))$speed_lim_smooth
+
+
+
+
 
 
 
